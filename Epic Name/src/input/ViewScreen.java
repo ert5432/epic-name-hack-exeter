f8f8bfe303d57b2d1d97537741d4a2996e918e04
Owner <Owner@172.25.20.108>
2016-04-30 18:23:56 -0400

    render floor

--- Epic Name/src/input/ViewScreen.java
+++ Epic Name/src/input/ViewScreen.java
@@ -4,6 +4,7 @@ import entities.Ant;
 import entities.Beholder;
 import entities.GameAgent;
 import entities.Goblin;
+import entities.GoblinFighter;
 import entities.Player;
 import entities.StateAgent;
 import geometry.Circle;
@@ -116,7 +117,7 @@ public class ViewScreen extends JPanel implements ActionListener{
 		};
 		g2.setWeapon(new PowerRod(world.player));
 		world.addEntity(g2);
-		g3=new Goblin(600,600,new Circle(32),world,new Stats(30,30,30,30,100));
+		g3=new GoblinFighter(600,600,world);
 		g3.setWeapon(new Sword(g3));
 		
 		StateMachine sm=((StateAgent)g3).getStateMachine();
