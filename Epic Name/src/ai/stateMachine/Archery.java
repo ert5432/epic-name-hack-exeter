be3927dbf8feede8f79e8f130019b80821d67164
Peter <peterminch575@gmail.com>
2016-04-30 16:28:13 -0400

    ai update

---
+++ Epic Name/src/ai/stateMachine/Archery.java
@@ -0,0 +1,45 @@
+package ai.stateMachine;
+
+import ai.steering.Behavior;
+import ai.steering.Pursue;
+import ai.steering.SteeringOutput;
+import entities.GameAgent;
+
+public class Archery extends State {
+
+	GameAgent target;
+	Behavior steer;
+	
+	public Archery(GameAgent agent, StateMachine sm,GameAgent target) {
+		super(agent, sm);
+		this.target=target;
+	}
+
+	@Override
+	public void enterState() {
+		// TODO Auto-generated method stub
+		steer=new Pursue(agent,target);
+		agent.setMaxSpeed(2);
+	}
+
+	@Override
+	public void execute() {
+		if(agent.canAct()&&agent.position.distanceTo(target.position)<300){
+			agent.act(1);
+		}
+		else{
+			SteeringOutput output=steer.getSteering();
+			agent.move(output.linear);
+		}
+		if(agent.position.distanceTo(target.position)>1000){
+			sm.changeState(new WaitForPlayer(agent,sm,target));
+		}
+	}
+
+	@Override
+	public void exitState() {
+		// TODO Auto-generated method stub
+		agent.setMaxSpeed(GameAgent.normalMaxSpeed);
+	}
+
+}
