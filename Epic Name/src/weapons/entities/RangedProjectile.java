b1de2fcd612451109077276ce0651b5afa2aef43
Owner <Owner@172.25.20.108>
2016-04-30 14:15:27 -0400

    thing

--- Epic Name/src/weapons/entities/RangedProjectile.java
+++ Epic Name/src/weapons/entities/RangedProjectile.java
@@ -1,6 +1,15 @@
 package weapons.entities;
 
 import world.World;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 import entities.GameAgent;
 import geometry.Shape;
 import geometry.Vector2D;
@@ -40,5 +49,34 @@ public class RangedProjectile extends Projectile{
 	
 	public double getAccuracyRange(){
 		return accuracyRange;
+	}
+	
+	public void render(Graphics g) {
+		//super.render(g);
+		System.out.println(rotation);
+		Graphics2D g2d = (Graphics2D) (g);
+		
+		g2d.rotate(rotation+owner.rotation+Math.PI,position.x,position.y);
+		
+		g2d.scale(3, 3);
+
+		System.out.println("sword");
+		
+		
+		try {
+			BufferedImage image = ImageIO.read(new File("res/arrow.png"));
+			
+			
+			
+			g2d.drawImage(image,(int)((position.x)/3.0),(int)((position.y+10)/3.0),null);
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		g2d.scale(1.0/3.0,1.0/3.0);
+		g2d.rotate(-owner.rotation-rotation-Math.PI,position.x,position.y);
 	}
 }
